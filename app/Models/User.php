fb31c15f9e937eecc9b4b69e99e36bebbccc8ad8
paresiqbal <thecrowdel@gmail.com>
2025-11-13 02:05:29 +0700

    Remove deprecated ExamQuestionAttachController, ExamQuestionController, Teacher's QuestionController, and related request and model files; clean up migration files for exam management

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,14 +10,8 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -25,11 +19,6 @@ class User extends Authenticatable
         'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'two_factor_secret',
@@ -37,11 +26,6 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
